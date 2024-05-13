a671cbbebb43d7fe3fedb69c7a56d90715124f6f
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-05-13 16:37:48 -0500

    new config

--- app/index.js
+++ app/index.js
@@ -1,5 +1,15 @@
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function Root() {
-  return <Link href="/home/messages">Navigate to nested route</Link>;
+  return (
+    <>
+      <Link href="/home">Navigate to nested route</Link>
+      <Pressable
+        onPress={() => router.push("/home/messages")}
+      >
+        <Text>Router push</Text>
+      </Pressable>
+    </>
+  );
 }
